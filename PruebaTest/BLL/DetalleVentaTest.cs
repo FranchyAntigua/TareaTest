e44cdca05e8e192ce76d3fcc6e5d6f5e502c5691
FranchyAntigua <Franchy_antigua@ucne.edu.do>
2019-06-21 19:10:42 -0400

    Programando Detalle de CompraTest y VentaTest

---
+++ PruebaTest/BLL/DetalleVentaTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaTest.BLL;
+using TareaTest.Entidades;
+
+namespace PruebaTest.BLL
+{
+    [TestClass()]
+    public class DetalleVentaTest
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Repositorio<DetalleVenta> repositorio = new Repositorio<DetalleVenta>();
+            bool paso = false;
+            DetalleVenta dv = new DetalleVenta();
+
+            dv.Iddetalleventa = 1;
+            dv.Idventa = 1;
+            dv.Idproducto = 1;
+            dv.Unidades = 1;
+            dv.Unidades = 5;
+            dv.Costounidad = 50;
+            dv.Descuentounidad = 0;
+            dv.Total = 250;
+            paso = repositorio.Guardar(dv);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Repositorio<DetalleVenta> repositorio = new Repositorio<DetalleVenta>();
+            bool paso = false;
+            DetalleVenta dv = repositorio.Buscar(1);
+            dv.Total = 260;
+            paso = repositorio.Modificar(dv);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
--- PruebaTest/BLL/DetallecompraTest.cs
+++ PruebaTest/BLL/DetallecompraTest.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TareaTest.BLL;
+using TareaTest.Entidades;
 
 namespace PruebaTest.BLL
 {
-   public class DetallecompraTest
-    {
+    [TestClass()]
+    public class DetallecompraTest
+   {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Repositorio<DetalleCompra> repositorio = new Repositorio<DetalleCompra>();
+            bool paso = false;
+            DetalleCompra dc = new DetalleCompra();
 
+            dc.Idcompra = 1;
+            dc.Iddetallecompra = 1;
+            dc.Idproducto = 1;
+            dc.Unidades = 1;
+            dc.Unidades = 5;
+            dc.Total = 1000;           
+            paso = repositorio.Guardar(dc);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Repositorio<DetalleCompra> repositorio = new Repositorio<DetalleCompra>();
+            bool paso = false;
+            DetalleCompra dc = repositorio.Buscar(1);
+            dc.Total = 1500;
+            paso = repositorio.Modificar(dc);
+            Assert.AreEqual(true, paso);
+        }
     }
 }
